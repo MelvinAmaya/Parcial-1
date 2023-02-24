067ea8797fbd2308a47891620a4d75c95d2d1076
MelvinPereira <pereiramelvin78@gmail.com>
2023-02-24 10:05:17 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+
+
+function adoptarPaquito(){
+    let nombre = prompt("Ingrese su nombre por favor.")
+
+    alert("Gracias "+nombre+" por adoptar a Paquito.")
+
+}
+
+function adoptarMinchito(){
+    let nombre = prompt("Ingrese su nombre por favor.")
+
+    alert("Gracias "+nombre+" por adoptar a Minchito.")
+
+}
+
+function adoptaManolo(){
+    let nombre = prompt("Ingrese su nombre por favor.")
+
+    alert("Gracias "+nombre+" por adoptar a Manolo.")
+    
+}
+
+function adoptaCapitan(){
+    let nombre = prompt("Ingrese su nombre por favor.")
+
+    alert("Gracias "+nombre+" por adoptar a Capitan.")
+    
+}
+
+
+
+function cambiocolor1(){
+    var color1 = document.getElementById("caja1")
+    color1.style.background = "#E8A40F"
+ }
+
+ function cambiocolor2(){
+    var color2 = document.getElementById("caja2")
+    color2.style.background = "#3DE80F"
+ }
+
+ function cambiocolor3(){
+    var color3 = document.getElementById("caja3")
+    color3.style.background = "#0FB5E8"
+ }
+
+ function cambiocolor4(){
+    var color4 = document.getElementById("caja4")
+    color4.style.background = "#9E0FE8"
+ }
+ function alert1(){
+    alert('ahora el color es naranja')
+ }
+
+ function alert2(){
+    alert('ahora el color es verde claro')
+ }
+
+ function alert3(){
+    alert('ahora el color es celste')
+ }
+
+ function alert4(){
+    alert('ahora el color es morado')
+ }
+
+ function reiniciar(){
+    var color4 = document.getElementById("caja1")
+    color4.style.background = "#ffff"
+    var color4 = document.getElementById("caja2")
+    color4.style.background = "#ffff"
+    var color4 = document.getElementById("caja3")
+    color4.style.background = "#ffff"
+    var color4 = document.getElementById("caja4")
+    color4.style.background = "#ffff"
+ }
